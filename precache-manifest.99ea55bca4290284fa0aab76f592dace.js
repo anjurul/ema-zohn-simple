716bbe48dceeaf60692fc3691208305ee92cb04f
anjurul <anjurul.ewallet@gmail.com>
2020-09-19 23:46:42 +0400

    Updates

---
+++ precache-manifest.99ea55bca4290284fa0aab76f592dace.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f80d11f6fd4c12037a547ae938b96e",
+    "url": "/ema-zohn-simple/index.html"
+  },
+  {
+    "revision": "16754f1054a6e4099459",
+    "url": "/ema-zohn-simple/static/css/main.3106d8dc.chunk.css"
+  },
+  {
+    "revision": "39242bbb4a65422c7686",
+    "url": "/ema-zohn-simple/static/js/2.d59f7996.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ema-zohn-simple/static/js/2.d59f7996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16754f1054a6e4099459",
+    "url": "/ema-zohn-simple/static/js/main.06877264.chunk.js"
+  },
+  {
+    "revision": "e9126745323a4d0854f3",
+    "url": "/ema-zohn-simple/static/js/runtime-main.49778bc7.js"
+  },
+  {
+    "revision": "e800c8469ca70ca84c718283496ee633",
+    "url": "/ema-zohn-simple/static/media/giphy.e800c846.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ema-zohn-simple/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "95f238a51a560680815e09e999af5cb9",
+    "url": "/ema-zohn-simple/static/media/logo.95f238a5.png"
+  }
+]);
